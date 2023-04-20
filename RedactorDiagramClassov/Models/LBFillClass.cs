629119a46305bac67f485a9829e14a0307d86fc3
Kagava <esspakov@mial.ru>
2023-04-20 23:27:24 +0700

    Ponyal

--- RedactorDiagramClassov/Models/LBFillClass.cs
+++ RedactorDiagramClassov/Models/LBFillClass.cs
@@ -8,6 +8,25 @@ namespace RedactorDiagramClassov.Models
 {
     public class LBFillClass
     {
-        
+        private string name { get; set; }
+        private string stereotip { get; set; }
+
+        private string vision { get; set; }
+
+        private string nameAt { get; set; }
+        private string tipAt { get; set; }
+        private string visionAt { get; set; }
+        private string stereotipsAt { get; set; }
+        private string defaultValueAt { get; set; }
+        private string specAt { get; set; }
+
+        private string nameOp { get; set; }
+        private string tipReturnValue { get; set; }
+        private string visionOp { get; set; }
+        private string paramNameOp { get; set; }
+        private string paramTipOp { get; set; }
+        private string specOp { get; set; }
+
+
     }
 }
